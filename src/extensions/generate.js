4aaff5c8385391d61e5ae4924c78530107a1c606
Corado M. Burcus <corado.burcus@osedea.com>
2019-05-23 12:20:17 -0400

    Fixed routes add issue and improved formatting when adding new routes

--- src/extensions/generate.js
+++ src/extensions/generate.js
@@ -103,40 +103,17 @@ module.exports = context => {
    * Adds routes to the config routes file for the backend
    */
   context.addRoutes = async (file, routes) => {
-    const routesFile = await filesystem.read(file)
+    const appRoutes = await filesystem.read(file, 'json')
 
-    // Anything past this index is not important
-    const routesEndIndex = routesFile.search(']; // Application routes')
-
-    // We'll put our new routes in between these two points
-    const preFile = routesFile.substring(0, routesEndIndex - 1)
-    const postFile = routesFile.substring(routesEndIndex, routesFile.length)
-
-    // Some checks to define how and where to add the new routes
-    const isFirstRoute = preFile.substring(preFile.length - 2, preFile.length) === '  '
-    const hasTrailingComma = preFile.charAt(preFile.length - 1) === ','
-
-    let newRoutes = ''
-
-    routes.forEach((route, index) => {
-      if (index === 0 && isFirstRoute) {
-        newRoutes += '{\n'
-      } else if (index === 0 && !hasTrailingComma) {
-        newRoutes += ', {\n'
-      } else {
-        newRoutes += ' {\n'
-      }
-
-      newRoutes += `    route: '${route.name}',\n`
-      newRoutes += `    path: '${route.path}',\n`
-      newRoutes += '    version: routeVersions.v1,\n'
-      newRoutes += '  },'
+    routes.forEach(route => {
+      appRoutes.push({
+        route: route.name,
+        path: route.path,
+        version: route.version || 'v1'
+      })
     })
 
-    newRoutes += '\n'
-
-    // Write the new routes to the file
-    await filesystem.write(file, preFile + newRoutes + postFile)
+    await filesystem.write(file, appRoutes)
 
     // Print the changes
     routes.forEach(route => {
